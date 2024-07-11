8654d137497988f10070b88548a9147caaafebc9
RachitSinghh <rachitsingh72@gmail.com>
2024-07-11 12:10:57 +0530

    Secured the DB key in .evn file

--- server.js
+++ server.js
@@ -3,27 +3,25 @@ const app = express();
 
 const db = require("./db");
 
+require("dotenv").config();
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Welcome to our hotel");
 });
 
-
-
-
 // Import the router file
 
-const personRoutes = require('./routes/PersonRoutes')
-const menuItem = require('./routes/menuItemRoutes')
+const personRoutes = require("./routes/PersonRoutes");
+const menuItem = require("./routes/menuItemRoutes");
 
 // use Router
-app.use('/person',personRoutes); 
-app.use('/menuItem', menuItem)
+app.use("/person", personRoutes);
+app.use("/menuItem", menuItem);
 
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("listening on Port 3000");
 });
